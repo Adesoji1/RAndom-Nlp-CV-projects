a21ad9b524c6224573b472c1da3d3269ed8fdadd
Adesoji Alu <adesoji.alu@gmail.com>
2022-12-14 00:34:20 +0100

    Update face_compare.py

--- Face Comparison/face_compare.py
+++ Face Comparison/face_compare.py
@@ -31,3 +31,90 @@ endpoint = FaceComparisonEndpoint()
 #Then, you could use it to compare an input image from the camera with an image in the database like this:
 result = endpoint(camera_image, database_image)
 #This would return "Matching" if the face in the input image matches the face in the database image, or "Not matching" if it does not
+
+##option 2 and 3 below
+
+"""
+write a face comparison  API Endpoint in pytorch that accepts input image from the camera in opencv and compares with the image in the database 
+
+
+
+import cv2
+import torch
+from torchvision import models
+
+# Set up the camera and get the input image
+camera = cv2.VideoCapture(0)
+ret, img = camera.read()
+
+# Preprocess the input image
+input_img = preprocess_image(img)
+
+# Load the pre-trained model
+model = models.resnet50(pretrained=True)
+
+# Use the model to extract features from the input image
+input_features = model(input_img)
+
+# Load the database of images and extract features for each image
+db_img_features = []
+for db_img in database_images:
+  db_img_preprocessed = preprocess_image(db_img)
+  db_img_features.append(model(db_img_preprocessed))
+
+# Compare the features of the input image with each image in the database
+# using a distance metric such as cosine similarity
+best_match = None
+best_similarity = 0
+for db_img_feature in db_img_features:
+  similarity = cosine_similarity(input_features, db_img_feature)
+  if similarity > best_similarity:
+    best_similarity = similarity
+    best_match = db_img
+
+# Return the best matching image from the database
+return best_match
+
+option 3 below
+
+
+import torch
+import cv2
+
+def face_comparison_api(image, database_img):
+    # Load the input image and database image into PyTorch tensors
+    input_img = torch.from_numpy(image)
+    db_img = torch.from_numpy(database_img)
+
+    # Use a pre-trained face detection model to detect faces in the input image
+    face_detection_model = ...
+    input_faces = face_detection_model(input_img)
+
+    # Use a pre-trained face recognition model to extract features from the detected faces
+    face_recognition_model = ...
+    input_features = face_recognition_model(input_faces)
+
+    # Use the same face recognition model to extract features from the face in the database image
+    db_features = face_recognition_model(db_img)
+
+    # Compare the extracted features using a distance function (e.g. cosine similarity)
+    similarity = ...
+
+    # Return the comparison result (e.g. True if similar, False if not)
+    return similarity > threshold
+
+# Use OpenCV to capture an image from the camera
+capture = cv2.VideoCapture(0)
+ret, frame = capture.read()
+
+# Load the image from the database
+database_img = cv2.imread('database_img.jpg')
+
+# Call the face comparison API
+is_match = face_comparison_api(frame, database_img)
+
+# Print the result
+print(is_match)
+
+
+"""
